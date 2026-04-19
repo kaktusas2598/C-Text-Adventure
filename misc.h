a8ebb1321e92c161d20047fded4d1476d5f0c635
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2026-04-19 19:42:24 +0100

    Comment explaining different types of distances

--- misc.h
+++ misc.h
@@ -1,15 +1,15 @@
 #include "object.h"
 
 typedef enum {
-    distSelf,
-    distHeld,
-    distHeldContained,
-    distLocation,
-    distHere,
-    distHereContained,
-    distOverthere,
-    distNotHere,
-    distUnknownObject
+    distSelf,           // The object is the player
+    distHeld,           // The object is being held by the player
+    distHeldContained,  // The object is being held by the player and contains something (eg. a bag)
+    distLocation,       // The object is the player's location
+    distHere,           // The object is present at the player's location    
+    distHereContained,  // Another object is present at the player's location and holding an object
+    distOverthere,      // The object is at nearby location
+    distNotHere,        // The object is(or appears to be) not here
+    distUnknownObject   // The parser did not recognize the noun entered
 } Distance;
 
 extern bool isHolding(Object* container, Object* obj);
